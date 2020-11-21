e132b49612101240fe49a126a120e8c231c4a354
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2020-11-22 03:22:13 +0700

    Center section.

--- src/animation-gallery/AnimationGallery.tsx
+++ src/animation-gallery/AnimationGallery.tsx
@@ -5,7 +5,7 @@ const componentList = [DotLoading]
 
 function AnimationGallery() {
   return (
-    <div className="container min-h-screen bg-green-200 bg-gradient-to-b from-green-100">
+    <div className="container mx-auto min-h-screen bg-green-200 bg-gradient-to-b from-green-100">
       <div className="grid grid-cols-6 gap-4 p-8">
         {componentList.map((Component) => (
           <div className="p-8 shadow-lg rounded text-center cursor-pointer bg-white">
