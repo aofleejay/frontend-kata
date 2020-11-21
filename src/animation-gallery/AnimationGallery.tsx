512af7383f77f0cf72e824716869294265bddef7
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2020-11-22 03:28:29 +0700

    Full width bg.

--- src/animation-gallery/AnimationGallery.tsx
+++ src/animation-gallery/AnimationGallery.tsx
@@ -5,14 +5,16 @@ const componentList = [DotLoading]
 
 function AnimationGallery() {
   return (
-    <div className="container mx-auto min-h-screen bg-green-200 bg-gradient-to-b from-green-100">
-      <div className="grid grid-cols-6 gap-4 p-8">
-        {componentList.map((Component) => (
-          <div className="p-8 shadow-lg rounded text-center cursor-pointer bg-white">
-            <p className="text-lg font-semibold mb-4">{Component.name}</p>
-            <Component />
-          </div>
-        ))}
+    <div className="min-h-screen bg-green-200 bg-gradient-to-b from-green-100">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-6 gap-4 p-8">
+          {componentList.map((Component) => (
+            <div className="p-8 shadow-lg rounded text-center cursor-pointer bg-white">
+              <p className="text-lg font-semibold mb-4">{Component.name}</p>
+              <Component />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
