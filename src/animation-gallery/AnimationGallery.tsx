0e88f3d4ad25c07f8a95dcdd49bed264adad9d2d
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2020-11-22 03:32:26 +0700

    Fix commponent name disapper.

--- src/animation-gallery/AnimationGallery.tsx
+++ src/animation-gallery/AnimationGallery.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import DotLoading from './DotLoading'
 
-const componentList = [DotLoading]
+const componentList = [{ title: 'Dot Loading', name: DotLoading }]
 
 function AnimationGallery() {
   return (
     <div className="min-h-screen bg-green-200 bg-gradient-to-b from-green-100">
       <div className="container mx-auto">
         <div className="grid grid-cols-6 gap-4 p-8">
-          {componentList.map((Component) => (
+          {componentList.map((component) => (
             <div className="p-8 shadow-lg rounded text-center cursor-pointer bg-white">
-              <p className="text-lg font-semibold mb-4">{Component.name}</p>
-              <Component />
+              <p className="text-lg font-semibold mb-4">{component.title}</p>
+              <component.name />
             </div>
           ))}
         </div>
